8420c8b803af6b06742b0c8de06628b8c06ebe06
Luke Trinity <ltrinity@uvm.edu>
2019-09-25 17:36:42 -0400

    comparing model accuracy test vs. train

--- tarExaminer.py
+++ tarExaminer.py
@@ -4,5 +4,40 @@ Created on Thu Sep 12 14:57:15 2019
 
 @author: luket
 """
-tarfile.open("root_data\\images.tar")
+#tarfile.open("root_data\\images.tar")
+from __future__ import absolute_import, division, print_function, unicode_literals
 
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+
+#this section needs to be recoded
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
