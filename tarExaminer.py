aea31f87f6917f6dcdb0e52c17dc0aad46b3b744
Luke Trinity <ltrinity@uvm.edu>
2019-09-26 21:39:53 -0400

    updating proj proposal and notes from recitation

--- tarExaminer.py
+++ tarExaminer.py
@@ -31,14 +31,39 @@ train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+#model = keras.Sequential([
+#    keras.layers.Flatten(input_shape=(28, 28)),
+#    keras.layers.Dense(128, activation=tf.nn.relu),
+#    keras.layers.Dense(10, activation=tf.nn.softmax)
+#])
+
+
+#https://medium.com/tensorflow/hello-deep-learning-fashion-mnist-with-keras-50fcff8cd74a
+
+#convolutional
+model = tf.keras.Sequential()
+# Must define the input shape in the first layer of the neural network
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=2, padding='same', activation='relu', input_shape=(28,28,1))) 
+model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
+model.add(tf.keras.layers.Dropout(0.3))
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
+model.add(tf.keras.layers.Dropout(0.3))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(256, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+# Take a look at the model summary
+model.summary()
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=5)
+#model.fit(train_images, train_labels, epochs=5)
+model.fit(train_images,
+         train_labels,
+         batch_size=64,
+         epochs=10,
+         validation_data=(test_images, test_labels))
+         callbacks=[checkpointer])
