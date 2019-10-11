985ad38decc291933b57cdb0e30c9adc31a44bda
Luke Trinity <ltrinity@uvm.edu>
2019-10-11 13:16:13 -0400

    successfully cropped an image with the supplied bounding box

--- tarExaminer.py
+++ tarExaminer.py
@@ -4,66 +4,68 @@ Created on Thu Sep 12 14:57:15 2019
 
 @author: luket
 """
-#tarfile.open("root_data\\images.tar")
 #https://www.tensorflow.org/tutorials/keras/basic_classification
 from __future__ import absolute_import, division, print_function, unicode_literals
-
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
-
 print(tf.__version__)
-
-fashion_mnist = keras.datasets.fashion_mnist
-
-
+import os
+# load and display an image with Matplotlib
+from matplotlib import image
+from matplotlib import pyplot
+# load image as pixel array
+data = image.imread('root_data/images/n02085620-Chihuahua/n02085620_7.jpg')
+f = open('root_data/Annotation/n02085620-Chihuahua/n02085620_7', "r")
+text = f.readlines()
+xmin = 0
+xmax = 250
+ymin = 0
+ymax = 250
+for line in text:
+    if(line[4:8]=="xmin"):
+        xmin = int(line[9:len(line)-8])
+    elif(line[4:8]=="xmax"):
+        xmax = int(line[9:len(line)-8])
+    elif(line[4:8]=="ymin"):
+        ymin = int(line[9:len(line)-8])
+    elif(line[4:8]=="ymax"):
+        ymax = int(line[9:len(line)-8])
+print(xmin,xmax,ymin,ymax)
+f.close()
+# load the image
+print(os.listdir())
+# summarize shape of the pixel array
+print(data.dtype)
+print(data.shape)
+# display the array of pixels as an image
+pyplot.imshow(data)
+pyplot.show()
+data = data[ymin:ymax, xmin:xmax, :]
+pyplot.imshow(data)
+pyplot.show()
+#https://medium.com/tensorflow/hello-deep-learning-fashion-mnist-with-keras-50fcff8cd74a
 #this section needs to be recoded
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-train_images = train_images / 255.0
-
-test_images = test_images / 255.0
-
+#(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+#
+#class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+#               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#
+#train_images = train_images / 255.0
+#
+#test_images = test_images / 255.0
+#
 #model = keras.Sequential([
 #    keras.layers.Flatten(input_shape=(28, 28)),
 #    keras.layers.Dense(128, activation=tf.nn.relu),
 #    keras.layers.Dense(10, activation=tf.nn.softmax)
 #])
-
-
-#https://medium.com/tensorflow/hello-deep-learning-fashion-mnist-with-keras-50fcff8cd74a
-
-#convolutional
-model = tf.keras.Sequential()
-# Must define the input shape in the first layer of the neural network
-model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=2, padding='same', activation='relu', input_shape=(28,28,1))) 
-model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
-model.add(tf.keras.layers.Dropout(0.3))
-model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=2))
-model.add(tf.keras.layers.Dropout(0.3))
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(256, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
-# Take a look at the model summary
-model.summary()
-
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
+#
+#model.compile(optimizer='adam',
+#              loss='sparse_categorical_crossentropy',
+#              metrics=['accuracy'])
+#
 #model.fit(train_images, train_labels, epochs=5)
-model.fit(train_images,
-         train_labels,
-         batch_size=64,
-         epochs=10,
-         validation_data=(test_images, test_labels))
-         callbacks=[checkpointer])
