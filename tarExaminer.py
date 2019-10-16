ac461951645b46d61c0e5d717b6cb05780280ba1
Luke Trinity <ltrinity@uvm.edu>
2019-10-16 16:45:59 -0400

    looping through multiple photos to crop and resize

--- tarExaminer.py
+++ tarExaminer.py
@@ -14,39 +14,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 print(tf.__version__)
 import os
+import PIL
 # load and display an image with Matplotlib
 from matplotlib import image
 from matplotlib import pyplot
-# load image as pixel array
-data = image.imread('root_data/images/n02085620-Chihuahua/n02085620_7.jpg')
-f = open('root_data/Annotation/n02085620-Chihuahua/n02085620_7', "r")
-text = f.readlines()
-xmin = 0
-xmax = 250
-ymin = 0
-ymax = 250
-for line in text:
-    if(line[4:8]=="xmin"):
-        xmin = int(line[9:len(line)-8])
-    elif(line[4:8]=="xmax"):
-        xmax = int(line[9:len(line)-8])
-    elif(line[4:8]=="ymin"):
-        ymin = int(line[9:len(line)-8])
-    elif(line[4:8]=="ymax"):
-        ymax = int(line[9:len(line)-8])
-print(xmin,xmax,ymin,ymax)
-f.close()
-# load the image
-print(os.listdir())
-# summarize shape of the pixel array
-print(data.dtype)
-print(data.shape)
-# display the array of pixels as an image
-pyplot.imshow(data)
-pyplot.show()
-data = data[ymin:ymax, xmin:xmax, :]
-pyplot.imshow(data)
-pyplot.show()
+#root directory
+rootDir = "root_data/images/n02085620-Chihuahua/"
+#list of image names
+imageNames = (os.listdir(rootDir))
+#dont blow up my computer
+i = 0
+#loop through each image in file
+for imgName in imageNames:
+    #for testing
+    if i < 5:
+        #grab the image
+        img =  PIL.Image.open(rootDir + imgName)
+        #get the bounding box annotation
+        f = open('root_data/Annotation/n02085620-Chihuahua/'+imgName[:-4], "r")
+        text = f.readlines()
+        xmin = 0
+        xmax = 250
+        ymin = 0
+        ymax = 250
+        for line in text:
+            if(line[4:8]=="xmin"):
+                xmin = int(line[9:len(line)-8])
+            elif(line[4:8]=="xmax"):
+                xmax = int(line[9:len(line)-8])
+            elif(line[4:8]=="ymin"):
+                ymin = int(line[9:len(line)-8])
+            elif(line[4:8]=="ymax"):
+                ymax = int(line[9:len(line)-8])
+        f.close()
+        #crop the image
+        cropped_img = img.crop((xmin,ymin,xmax,ymax))
+        #resize the image
+        resized_img = cropped_img.resize((250, 250))
+        #show the image
+        resized_img.show()
+    i+=1
 #https://medium.com/tensorflow/hello-deep-learning-fashion-mnist-with-keras-50fcff8cd74a
 #this section needs to be recoded
 #(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
