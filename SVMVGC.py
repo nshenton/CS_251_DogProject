159f3da50038ca5aa2d4834cec753e28ec9b842c
Luke Trinity <ltrinity@uvm.edu>
2019-11-19 23:21:12 -0500

    implemented basic neural network, still havent beat svm performance

--- SVMVGC.py
+++ SVMVGC.py
@@ -28,6 +28,8 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from scipy.stats import uniform
 from scipy.stats import randint
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 # Utility function to report best scores
 #takes params results (from random search cv) and # to report (default 1)
 def report(results, n_top=1):
@@ -94,7 +96,7 @@ def generateXy():
     print(y)   
     return X,y
 #this class takes in the train and test data matrix and a param identifying what type of model to test on
-def splitAndTest(X, y, svmMod):
+def splitAndTest(X, y, modelToUse):
     # specify parameters and distributions to sample from (SVM)
     param_dist_svm = {"C": expon(scale=1000),
                  "gamma": expon(scale=2),
@@ -102,41 +104,59 @@ def splitAndTest(X, y, svmMod):
                  "class_weight":["balanced",None]}
     # specify parameters and distributions to sample from (random forest)
     param_dist_rf = {"min_samples_split": uniform(),
-                     "max_depth":randint(1,5)}
+                     "max_depth":randint(1,50)}
     
     # run randomized search
-    n_iter_search = 2000
+    n_iter_search = 250
     
-    if svmMod: 
+    if modelToUse == 0: 
         model = svm.SVC()
         random_search = RandomizedSearchCV(model, param_distributions=param_dist_svm,
-                                       n_iter=n_iter_search, cv=3, iid=False)
-    else:   
-        model = RandomForestClassifier(n_estimators=100)
+                                       n_iter=n_iter_search, cv=20, iid=False)
+    elif modelToUse == 1: 
+        model = RandomForestClassifier(n_estimators=10)
         random_search = RandomizedSearchCV(model, param_distributions=param_dist_rf,
-                               n_iter=n_iter_search, cv=3, iid=False)
+                               n_iter=n_iter_search, cv=5, iid=False)
+    elif modelToUse == 2:
+        # define the keras model
+        bestModel = Sequential()
+        bestModel.add(Dense(12, input_dim=2, activation='relu'))
+        bestModel.add(Dense(8, activation='relu'))
+        bestModel.add(Dense(4, activation='relu'))
+        bestModel.add(Dense(1, activation='sigmoid'))
+        bestModel.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     
     #tune params and report accuracy n times
     for i in range(5):
-        start = time()
         #split
-        X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=random.randint(0,10000))
-        #fit on training set
-        random_search.fit(X_train, y_train)
-        #report results
-        print("RandomizedSearchCV took %.2f seconds for %d candidates"
-              " parameter settings." % ((time() - start), n_iter_search))
-        report(random_search.cv_results_)
+        X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=random.randint(0,10000))
         #tune parameters
-        if svmMod:
+        if modelToUse == 0:
+            start = time()
+            #fit on training set
+            random_search.fit(X_train, y_train)
+            #report results
+            print("RandomizedSearchCV took %.2f seconds for %d candidates"
+                  " parameter settings." % ((time() - start), n_iter_search))
+            report(random_search.cv_results_)
             bestModel = svm.SVC(C = random_search.best_params_['C'],
                                 class_weight = random_search.best_params_['class_weight'],
                                 gamma = random_search.best_params_['gamma'],
                                 kernel = random_search.best_params_['kernel'])
-        else:
-            bestModel = RandomForestClassifier(n_estimators=100, max_depth=random_search.best_params_['max_depth'], 
+            bestModel.fit(X_train,y_train)
+        elif modelToUse == 1:
+            start = time()
+            #fit on training set
+            random_search.fit(X_train, y_train)
+            #report results
+            print("RandomizedSearchCV took %.2f seconds for %d candidates"
+                  " parameter settings." % ((time() - start), n_iter_search))
+            report(random_search.cv_results_)
+            bestModel = RandomForestClassifier(n_estimators=10, max_depth=random_search.best_params_['max_depth'], 
                                            min_samples_split = random_search.best_params_['min_samples_split'])
-        bestModel.fit(X_train,y_train)
+            bestModel.fit(X_train,y_train)
+        elif modelToUse == 2:
+            history = bestModel.fit(X_train, y_train, epochs=500, batch_size=500, verbose = 0)
         # Plot the decision boundary for the test data
         #Source: https://stackoverflow.com/questions/22294241/plotting-a-decision-boundary-separating-2-classes-using-matplotlibs-pyplot
         h = .001  # step size in the mesh
@@ -157,7 +177,29 @@ def splitAndTest(X, y, svmMod):
         plt.contour(xx, yy, Z, cmap=plt.cm.flag)
         plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, s=40, cmap=plt.cm.flag);
         plt.show()
-        acc = metrics.accuracy_score(bestModel.predict(X_test),y_test)
-        print("Accuracy: " + str(acc))
+        if modelToUse == 2:
+            indH, accuracy = bestModel.evaluate(X_test, y_test)
+            print('Accuracy: %.2f' % (accuracy*100))
+            #performance metrics
+            acc = history.history['accuracy']
+            
+            loss = history.history['loss']
+            
+            epochs_range = range(500)
+            
+            plt.figure(figsize=(8, 8))
+            plt.subplot(1, 2, 1)
+            plt.plot(epochs_range, acc, label='Training Accuracy')
+            plt.legend(loc='lower right')
+            plt.title('Training Accuracy')
+            
+            plt.subplot(1, 2, 2)
+            plt.plot(epochs_range, loss, label='Training Loss')
+            plt.legend(loc='upper right')
+            plt.title('Training Loss')
+            plt.show()
+        else:
+            acc = metrics.accuracy_score(bestModel.predict(X_test),y_test)
+            print("Accuracy: " + str(acc))
 #X,y = generateXy()
-splitAndTest(X,y, True)
+splitAndTest(X,y,2)
